1c63737901475597eb18fa676e62173fa79e86e5
2417946 <2417946@LTIN524009>
2025-06-30 22:26:37 +0530

    Add project files.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCoreBasic.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=LTIN524009\\SQLEXPRESS;Database=Product;Trusted_Connection=True;Encrypt=True;TrustServerCertificate=True");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using EFCoreBasic.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreBasic
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            SaveData();
+            FetchData();
+            Console.WriteLine("Hello, World!");
+        }
+        static void SaveData()
+        {
+            using (var dbContext = new AppDbContext())
+
+            {
+
+                var newProduct = new Product { Name = "New Product", price = 9 };
+
+                dbContext.Products.Add(newProduct);
+
+                dbContext.SaveChanges();
+
+            }
+        }
+        static void FetchData()
+        {
+            using (var dbContext = new AppDbContext())
+
+            {
+
+                var products = dbContext.Products.ToList(); // Retrieve all products 
+
+
+
+                // Display the products 
+
+                foreach (var product in products)
+
+                {
+
+                    Console.WriteLine($"Product ID: {product.ProductId}, Name: {product.Name}, Price: {product.Price}");
+
+                }
+
+            }
+        }
+    }
+}
